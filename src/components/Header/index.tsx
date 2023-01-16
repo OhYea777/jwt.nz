9e3a5a4edae0a705097a0e0ad456f05d6a2b5fb7
OhYea777 <jack@jwt.nz>
2023-01-17 09:28:21 +1300

    Fix docker build

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,18 @@
+import Image from 'next/image';
+
 export default function Header() {
   return (
     <>
       <header>
         <div className="flex w-full p-6">
           <div className="flex items-center justify-between">
-            <img className="h-8 mr-5" src="/logo.svg" alt="Logo" />
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={32}
+              height={32}
+              className="mr-5"
+            />
             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white xs:hidden md:inline-block">
               Jack Taylor
             </span>
